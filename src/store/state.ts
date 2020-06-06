bfca55db12ed05161501068d0f7d9610434b8f8e
ru_shalm <ru_shalm@hazimu.com>
2020-06-06 16:19:57 +0900

    add samples

--- src/store/state.ts
+++ src/store/state.ts
@@ -1,4 +1,5 @@
 import { RenderParameter } from '../types/RenderParameter';
+import { sampleImage } from '../assets/sample';
 
 export interface RootState {
   renderParameter: RenderParameter;
@@ -7,13 +8,18 @@ export interface RootState {
 
 export const initialState: RootState = {
   renderParameter: {
-    pictures: [],
-    width: 640,
-    height: 360,
+    pictures: [
+      {
+        id: 1,
+        url: sampleImage,
+      },
+    ],
+    width: 1200,
+    height: 630,
     itemSizeX: 5,
     itemSizeY: 5,
-    itemWidth: 150,
-    itemHeight: 100,
+    itemWidth: 320,
+    itemHeight: 180,
     rotation: -8,
     margin: 10,
     borderRadius: 5,
