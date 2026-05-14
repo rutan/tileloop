40ab3a6f4dd6fcf967f94d089c75b87f757a5411
rutan <ru_shalm@hazimu.com>
2026-05-15 02:23:14 +0900

    replace sample

--- src/store/state.ts
+++ src/store/state.ts
@@ -1,4 +1,7 @@
-import { sampleImage } from '../assets/sample';
+import sampleImage01 from '../assets/01.jpg';
+import sampleImage02 from '../assets/02.jpg';
+import sampleImage03 from '../assets/03.jpg';
+import sampleImage04 from '../assets/04.jpg';
 import { RenderParameter } from '../types/RenderParameter';
 
 export interface RootState {
@@ -10,14 +13,26 @@ export const initialState: RootState = {
   renderParameter: {
     pictures: [
       {
-        id: 'sample',
-        url: sampleImage,
+        id: 'sample1',
+        url: sampleImage01,
+      },
+      {
+        id: 'sample2',
+        url: sampleImage02,
+      },
+      {
+        id: 'sample3',
+        url: sampleImage03,
+      },
+      {
+        id: 'sample4',
+        url: sampleImage04,
       },
     ],
     width: 1200,
     height: 630,
-    itemWidth: 300,
-    itemHeight: 180,
+    itemWidth: 240,
+    itemHeight: 240,
     rotation: -6,
     margin: 18,
     borderRadius: 12,
